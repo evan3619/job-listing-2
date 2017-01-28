b8cf0253c00becece8924a3f28e46860ac979789
evan3619 <evan3619@163.com>
2017-01-28 22:42:33 +0800

    implement part 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         def admin?
+             email == 'user1@163.com'
+
+           end
+           def admin?
+              is_admin
+            end
 end
