2f297add61318eb5b6596c0492d3035625d0b605
evan3619 <evan3619@163.com>
2017-01-28 22:46:28 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,4 @@
-
-	
+class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_filter :require_is_admin
   def show
@@ -44,7 +43,12 @@
 
     redirect_to admin_jobs_path
   end
- 
+ def require_is_admin
+     if !current_user.admin?
+       flash[:alert] = 'You are not admin'
+       redirect_to root_path
+     end
+   end
   private
 
   def job_params
