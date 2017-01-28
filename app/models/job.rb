2f43f621b7980d1154f75f4cd3bfb47e8184097e
evan3619 <evan3619@163.com>
2017-01-29 00:08:31 +0800

    step 7 upload resume

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,10 +3,11 @@ class Job < ApplicationRecord
 	validates :wage_upper_bound, presence: true
 	validates :wage_lower_bound, presence: true
 	validates :wage_lower_bound, numericality: { greater_than: 0}
-	
+
 	scope :recent, -> { order('created_at DESC') }
-	
 	scope :published, -> { where(is_hidden: false) }
+    
+    has_many :resumes
   def publish!
     self.is_hidden = false
     self.save
@@ -16,5 +17,5 @@ class Job < ApplicationRecord
     self.is_hidden = true
     self.save
   end
-
+ 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          def admin?
              email == 'user1@163.com'
 
            end
-           def admin?
-              is_admin
-            end
+         def admin?
+          is_admin
+         end
+  
+   has_many :resumes
 end
